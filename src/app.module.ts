fc8678ce08865fbd4df9d9a6af94cf14c5ea233a
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-08-25 10:40:33 +0530

    envOptions config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthMiddleware } from './middlewares/auth.middleware';
-import { envOptions } from './config/envoptions';
+import { envOptions } from './config/envOptions';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 import { RoomBookingModule } from './room-booking/room-booking.module';
